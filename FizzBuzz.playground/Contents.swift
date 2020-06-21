5b3565e695c65fba6591616dbc94c73410064597
RobertJG17 <robertjguerra@yahoo.com>
2020-06-21 02:38:41 -0700

    Added comments describing depth of performance benchmarks of both approaches

--- FizzBuzz.playground/Contents.swift
+++ FizzBuzz.playground/Contents.swift
@@ -1,3 +1,23 @@
+//Comparison between if-else flow and switch statements to achieve the same objective.
+//It's worth noting that I could have implemented a more efficient if-else block but as
+//the code stands, the switch case wins in terms of efficiency and memory management as per
+// LEET code diagnostics.
+
+
+//For if-else code:
+//
+//Runtime: 28 ms, faster than 35.53% of Swift online submissions for Fizz Buzz.
+//Memory Usage: 21.3 MB, less than 22.31% of Swift online submissions for Fizz Buzz.
+
+
+//For switch code:
+//
+//Runtime: 20 ms, faster than 98.68% of Swift online submissions for Fizz Buzz.
+//Memory Usage: 21.1 MB, less than 64.94% of Swift online submissions for Fizz Buzz.
+
+//**Note: Although the perfomance gap seems wide, I noticed that running the same code several
+//times resulted in very different performance benchmarks.
+
 import UIKit
 
 /// FizzBuzz using continuous string appends
