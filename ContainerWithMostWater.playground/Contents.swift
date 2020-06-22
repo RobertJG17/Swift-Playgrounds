69d58f882d0fdcf6ca8d556867665a2fd8406ce1
RobertJG17 <robertjguerra@yahoo.com>
2020-06-21 20:22:33 -0700

    Code Refactor and UpdatedSolution

--- ContainerWithMostWater.playground/Contents.swift
+++ ContainerWithMostWater.playground/Contents.swift
@@ -9,44 +9,39 @@ import UIKit
 
 func maxArea(_ height: [Int]) -> Int {
     
-    var maxProducts = [Int]()
-    var iterator = 0
+    var products = [Int] ()
+    var numOfInterest: Int
+    var difference: Int
+    var heightCopy = height
     
-    //Ensuring the list is not empty
+    while !heightCopy.isEmpty {
+        numOfInterest = heightCopy.remove(at: 0)
+        difference = 1
+        for num in heightCopy {
+            products.append(min(numOfInterest, num) * difference)
+            difference += 1
+        }
+
+        if heightCopy.count == 1 {
+            break
+        }
+    }
+    
+    //Since function specifies definitive return value,
+    //we must return 0 as apposed to nil if the height
+    //list is empty.
     if height.isEmpty { return 0 }
     
-    //Ensuring the list does not contain all of the same value (if let unwrap block will not execute)
-    let heightSet = Set(height)
-    if heightSet.count == 1 {
-        return (height.count - 1) * height[0]
-    }
-    
-    //Once the two conditions above have been checked, the code can run normal operations
-    while iterator < height.count {
-        let numOfInterest = [height[iterator], iterator]
-        var products = [Int]()
-        
-        for num in height {
-            if let index = height.firstIndex(of: num) {
-                if [num, index] == numOfInterest {
-                    continue
-                }
-
-                products.append(abs(min(numOfInterest[0], num) * (index - numOfInterest[1])))
-            }
-        }
-        maxProducts.append(products.max()!)
-        iterator += 1
-        print(height)
-    }
-    
-    return maxProducts.max()!
+    return products.max()!
 }
 
 print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
-//print(maxArea([5, 1, 3, 4, 2, 6]))
-//print(maxArea([1, 500,000, 6, 2, 5, 4, 8, 3, 7]))
-//print(maxArea([]))
-//print(maxArea([4, 4, 4, 4]))
-//print(maxArea([1, 1]))
-//print(maxArea([1, 2, 1]))
+print(maxArea([5, 1, 3, 4, 2, 6]))
+print(maxArea([1, 500,000, 6, 2, 5, 4, 8, 3, 7]))
+print(maxArea([]))
+print(maxArea([4, 4, 4, 4]))
+print(maxArea([1, 1]))
+print(maxArea([1, 2, 1]))
+print(maxArea([3, 6, 5, 4]))
+
+
